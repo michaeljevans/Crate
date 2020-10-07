625aa1986dd0b0e0ee1671546d50f65588c53bbd
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-07 16:33:10 -0600

    Add comments for routing on app render

--- code/web/src/setup/client/App.js
+++ code/web/src/setup/client/App.js
@@ -8,13 +8,21 @@ import Layout from '../../modules/common/Layout'
 import NotFound from '../../modules/common/NotFound'
 import RoutePrivate from '../../modules/auth/RoutePrivate'
 
+// checking to see what the mapped routes look like. CAN DELETE CONSOLE LOG LATER!
+console.log(Object.values(routes).map(route => console.log('route:', route, 'route path:', route.path)))
+
 const App = () => (
   <Layout>
     <Switch>
+      {/* maps over each object exported from files in the routes */}
       {Object.values(routes).map((route, index) => (
-        route.auth
+        route.auth 
+        // if the route has a property called auth with truthy value then return this code...
           ? <RoutePrivate {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
-          : <Route {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
+          : 
+          // OTHERWISE return this code...
+          // DOES STYLESFORM NEED AUTH PROPERTY??????
+          <Route {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
       ))}
 
       <Route component={NotFound}/>
