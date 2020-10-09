fffde966284a7737ac522310180048822fdecd27
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-09 07:58:04 -0600

    Add annotations

--- code/web/webpack.config.babel.js
+++ code/web/webpack.config.babel.js
@@ -2,6 +2,8 @@
 import path from 'path'
 import Dotenv from 'dotenv-webpack'
 
+
+// specifying an entry file (index.js), webpack reads all the imports in the application, starting from this file, building like a tree.
 const config = {
   entry: {
     app: './src/setup/client/index.js'
@@ -11,7 +13,7 @@ const config = {
     path: path.join(__dirname, 'public', 'js', 'bundles'),
     filename: '[name].js'
   },
-
+/* for every file webpack finds, it needs to follow some rules */
   module: {
     rules: [
       {
