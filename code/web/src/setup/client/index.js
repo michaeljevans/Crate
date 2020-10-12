4be86a8692425192be8a7ccd4678a78d9f20399f
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-11 18:51:54 -0600

    Add notes on user authentication when app loads

--- code/web/src/setup/client/index.js
+++ code/web/src/setup/client/index.js
@@ -11,18 +11,26 @@ import ScrollToTop from '../../modules/common/ScrollToTop'
 import App from './App'
 
 // User Authentication
+// if user logs in there is a token to get from local storage? YES!!
+// In addition to a users details, a token is returned from the resolver method (login) on the backend. 
+// When a user logs in successfully the token and user are added to local storage
 const token = window.localStorage.getItem('token')
 if (token && token !== 'undefined' && token !== '') {
+  // console.log('Token:', token)
   const user = JSON.parse(window.localStorage.getItem('user'))
   if (user) {
-    // Dispatch action
+    // Dispatch action through setUser method(?) 
+    // setUser returns SET_USER action with payload of the user object, which adds the user to store
+    // is this how a user remains logged in even if they close the window?
     store.dispatch(setUser(token, user))
+    // console.log('User:', user)
 
     loginSetUserLocalStorageAndCookie(token, user)
   }
 }
 
 // Client App
+// wrapping the APP with <Provider > connects the APP to Redux store
 const Client = () => (
   <Provider store={store} key="provider">
     <Router>
@@ -34,6 +42,7 @@ const Client = () => (
 )
 
 // Mount client app
+/* when window loads, render this code inside the 'app' html element */
 window.onload = () => {
   hydrate(
     <Client/>,
