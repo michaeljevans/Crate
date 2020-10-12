45a3c1171c851eb0c082d21fc66cced7a797529a
Greyson Elkins <greysonelkins@gmail.com>
2020-10-12 17:06:29 -0600

    Create test-survey object

---
+++ code/web/src/modules/common/surveys/example-survey.js
@@ -0,0 +1,44 @@
+import image from '../../../../public/images/joke-shirt.jpg'
+
+export const testSurvey = [{
+  question: 'Which shirt would make you feel the most confident at work?',
+  images: [{
+    src: image, value: 'this', description: 'the best t-shirt'
+  }, {
+    src: image, value: 'that', description: 'the next best t-shirt'
+  }, {
+    src: image, value: 'the other', description: 'the third best t-shirt'
+  }],
+  answer: null
+  }, {
+  question: 'Which one of these shirts do you own the most of?',
+  images: [{
+    src: image, value: 'this', description: 'the best t-shirt'
+  }, {
+    src: image, value: 'that', description: 'the next best t-shirt'
+  }, {
+    src: image, value: 'the other', description: 'the third best t-shirt'
+  }],
+  answer: null
+  }, {
+  question: 'What shirt is the most fitting?',
+  images: [{
+    src: image, value: 'this', description: 'the best t-shirt'
+  }, {
+    src: image, value: 'that', description: 'the next best t-shirt'
+  }, {
+    src: image, value: 'the other', description: 'the third best t-shirt'
+  }],
+  answer: null
+  }, {
+  question: 'Which one would you be most likely to buy your father for his birthday?',
+  images: [{
+    src: image, value: 'this', description: 'the best t-shirt'
+  }, {
+    src: image, value: 'that', description: 'the next best t-shirt'
+  }, {
+    src: image, value: 'the other', description: 'the third best t-shirt'
+  }],
+  answer: null
+  }
+]
