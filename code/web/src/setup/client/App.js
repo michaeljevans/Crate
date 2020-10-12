45ad530774da87f5dc4a14936770c9203350598b
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-11 18:39:55 -0600

    Edit notes on rendering the different routes

--- code/web/src/setup/client/App.js
+++ code/web/src/setup/client/App.js
@@ -8,23 +8,21 @@ import Layout from '../../modules/common/Layout'
 import NotFound from '../../modules/common/NotFound'
 import RoutePrivate from '../../modules/auth/RoutePrivate'
 
-// checking to see what the mapped routes look like. CAN DELETE CONSOLE LOG LATER!
-console.log(Object.values(routes).map(route => console.log('route:', route, 'route path:', route.path)))
-
 const App = () => (
   <Layout>
     <Switch>
-      {/* maps over each object exported from files in the routes */}
-      {Object.values(routes).map((route, index) => (
+      {/* maps over each object exported from files in the routes directory*/}
+      { 
+      Object.values(routes).map((route, index) => (
         route.auth 
-        // if the route has a property called auth with truthy value then return this code...
+        // if the route has a property called auth with truthy value then render RoutePrivate for that route
+        // I think that the styleSurvey route will need to be RoutePrivate as we only want users to see it if they've signed up to app, AND are logged in
           ? <RoutePrivate {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
           : 
-          // OTHERWISE return this code...
-          // DOES STYLESFORM NEED AUTH PROPERTY??????
+          // OTHERWISE.. render Route 
           <Route {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
       ))}
-
+      
       <Route component={NotFound}/>
     </Switch>
   </Layout>
