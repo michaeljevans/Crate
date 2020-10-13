104a7fbb0e9f54e353fdbb7f595ae8f9d1e41032
Greyson Elkins <greysonelkins@gmail.com>
2020-10-13 17:57:46 -0600

    Use Card components in Survey component

--- code/web/src/modules/style/Survey.js
+++ code/web/src/modules/style/Survey.js
@@ -8,13 +8,15 @@ import { Helmet } from 'react-helmet'
 
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
+import Card from '../../ui/card/Card'
 // import Button from '../../ui/button'
 // import ImageTile from '../../ui/image/Tile'
 // import Input from '../../ui/input/Input'
 import H3 from '../../ui/typography/H3'
+import H4 from '../../ui/typography/H4'
 // import Icon from '../../ui/icon'
 // import { level1 } from '../../ui/common/shadows'
-import { grey } from '../../ui/common/colors'
+import { black, white, grey } from '../../ui/common/colors'
 
 // App Imports
 import { APP_URL } from '../../setup/config/env'
@@ -27,14 +29,16 @@ class Survey extends Component {
   }
 
   buildSurvey = () => {
-    survey.map(question => {
-      <div>
-        <h2>question.question</h2>
-        {question.images.map(answer => {
-          <img src={APP_URL + answer.src} alt={answer.description} />
-        })}
-      </div>
-    })
+
+    return survey.map(question => (
+        <Card style={{width: '75em', margin: '2.5em auto', backgroundColor: white }}>
+          <H4 font="secondary" style={{ color: black }}>question.question</H4>
+          {question.images.map(answer => {
+            <img src={APP_URL + answer.src} alt={answer.description} />
+          })}
+        </Card>
+      )
+    )
   }
 
   render() {
@@ -43,7 +47,7 @@ class Survey extends Component {
         <Helmet>
           <title>Style Survey</title>
         </Helmet>
-
+        
         <Grid style={{ backgroundColor: grey }}>
           <GridCell style={{ padding: '2em', textAlign: 'center' }}>
             <H3 font="secondary">Tell us about your style!</H3>
@@ -51,7 +55,6 @@ class Survey extends Component {
               {/* subscribe to multiple crates.</p> */}
           </GridCell>
         </Grid>
-
         <Grid>
           <GridCell>
             {
@@ -59,7 +62,6 @@ class Survey extends Component {
             }
           </GridCell>
         </Grid>
-
       </div>
     )
   }
