357fec6365c4e4efd5379d6bb5fdf0b185648fa0
Greyson Elkins <greysonelkins@gmail.com>
2020-10-12 20:32:32 -0600

    Build rough survey template #39

--- code/web/src/modules/style/Survey.js
+++ code/web/src/modules/style/Survey.js
@@ -18,10 +18,49 @@ import { Helmet } from 'react-helmet'
 // App Imports
 import { APP_URL } from '../../setup/config/env'
 import { messageShow, messageHide } from '../common/api/actions'
+import {testSurvey as survey} from '../../../src/modules/common/surveys/example-survey'
 
 class Survey extends Component {
   constructor(props) {
     super(props) 
+  }
+
+  buildSurvey = () => {
+    testSurvey.map(question => {
+      <div>
+        <h2>question.question</h2>
+        {question.answers.map(answer => {
+          <img src={APP_URL + answer.src} alt={answer.description} />
+        })}
+      </div>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>Style Survey</title>
+        </Helmet>
+
+        <Grid style={{ backgroundColor: grey }}>
+          <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+            <H3 font="secondary">Tell us about your style!</H3>
+            {/* <p style={{ marginTop: '1em', color: grey2 }}>You can choose crate which suits your need. You can also */}
+              {/* subscribe to multiple crates.</p> */}
+          </GridCell>
+        </Grid>
+
+        <Grid>
+          <gridCell>
+            {
+              this.buildSurvey()
+            }
+          </gridCell>
+        </Grid>
+
+      </div>
+    )
   }
 }
 
