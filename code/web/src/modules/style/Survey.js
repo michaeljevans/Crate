e9cd88503f075f61c619c58db0b29dcb8becd48a
Greyson Elkins <greysonelkins@gmail.com>
2020-10-13 18:23:32 -0600

    Add missing img return, render Q&As #39

--- code/web/src/modules/style/Survey.js
+++ code/web/src/modules/style/Survey.js
@@ -4,18 +4,12 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
-// import { Grid, GridCell} from '../../ui/grid'
 
 // UI Imports
 import { Grid, GridCell } from '../../ui/grid'
 import Card from '../../ui/card/Card'
-// import Button from '../../ui/button'
-// import ImageTile from '../../ui/image/Tile'
-// import Input from '../../ui/input/Input'
 import H3 from '../../ui/typography/H3'
 import H4 from '../../ui/typography/H4'
-// import Icon from '../../ui/icon'
-// import { level1 } from '../../ui/common/shadows'
 import { black, white, grey } from '../../ui/common/colors'
 
 // App Imports
@@ -29,13 +23,12 @@ class Survey extends Component {
   }
 
   buildSurvey = () => {
-
     return survey.map(question => (
         <Card style={{width: '75em', margin: '2.5em auto', backgroundColor: white }}>
-          <H4 font="secondary" style={{ color: black }}>question.question</H4>
-          {question.images.map(answer => {
+          <H4 font="secondary" style={{ color: black }}>{question.question}</H4>
+          {question.images.map(answer => (
             <img src={APP_URL + answer.src} alt={answer.description} />
-          })}
+          ))}
         </Card>
       )
     )
