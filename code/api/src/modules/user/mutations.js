08b7c62453a67feb7b54d0b3d18eea06b9fdd96e
Josh Tukman <josh.tukman@gmail.com>
2020-10-13 19:36:41 -0600

    Import 'updateUser' from Resolver and create 'userUpdate' variable

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -3,7 +3,7 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
 import { UserType } from './types'
-import { create, remove, update } from './resolvers'
+import { create, remove, updateUser} from './resolvers'
 
 // Create
 export const userSignup = {
@@ -27,23 +27,6 @@ export const userSignup = {
   resolve: create
 }
 
-// Update
-export const userUpdate = {
-  type: UserType,
-  args: {
-    id: {
-      name: 'id',
-      type: GraphQLInt
-    },
-
-    style: {
-      name: 'style',
-      type: GraphQLString
-    }
-  },
-  resolve: update
-}
-
 // Remove
 export const userRemove = {
   type: UserType,
@@ -54,4 +37,20 @@ export const userRemove = {
     }
   },
   resolve: remove
+}
+
+//Update
+export const userUpdate = {
+  type: UserType,
+  args: {
+    id: {
+      name: 'id',
+      type: GraphQLInt
+    },
+    style: {
+      name: 'style',
+      type: GraphQLString
+    }
+  },
+  resolve: updateUser
 }
