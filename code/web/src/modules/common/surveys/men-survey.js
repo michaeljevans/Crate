b39989e3bb769bb8c03891da27661c27173540e6
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-14 16:46:42 -0600

    Add images and text to survey

--- code/web/src/modules/common/surveys/men-survey.js
+++ code/web/src/modules/common/surveys/men-survey.js
@@ -1,31 +1,37 @@
 export const mensSurvey = [{
   question: 'Which outfit would make you feel the most confident at work?',
   images: [{
-    src: '/images/joke-shirt.jpg', value: 'this', description: 'the best t-shirt'
-  }, {
-    src: '/images/joke-shirt.jpg', value: 'that', description: 'the next best t-shirt'
-  }, {
-    src: '/images/joke-shirt.jpg', value: 'the other', description: 'the third best t-shirt'
+    src: '/images/mensSurvey/work-bold-mens.jpeg', value: 'bold', description: 'mens blazor with colorful design'
+    }, {
+    src: '/images/mensSurvey/work-classic-mens.jpeg', value: 'classic', description: 'classic suit jacket with collared white shirt and vest underneath'
+    }, {
+    src: '/images/mensSurvey/work-trendy-mens-work.jpeg', value: 'trendy', description: 'casual but tidy black pants and olive green jacket'
+    }, {
+      src: '/images/mensSurvey/work-sporty-mens-work.jpg', value: 'sporty', description: 'gray t-shirt and tidy track pants'
   }],
   answer: null
 }, {
-  question: 'Which style would you most likely wear on a date night?',
-  images: [{
-    src: '/images/joke-shirt.jpg', value: 'this', description: 'the best t-shirt'
-  }, {
-    src: '/images/joke-shirt.jpg', value: 'that', description: 'the next best t-shirt'
-  }, {
-    src: '/images/joke-shirt.jpg', value: 'the other', description: 'the third best t-shirt'
-  }],
+  question: 'Which outfit would you most likely wear on a date night?',
+    images: [{
+      src: '/images/mensSurvey/date-classic-mens.jpeg', value: 'classic', description: 'tailored pants and fitted collared t-shirt'
+      }, {
+      src: '/images/mensSurvey/date-bold-mens.jpeg', value: 'bold', description: 'leopard print suit jacket and shorts with matching tie'
+      }, {
+      src: '/images/mensSurvey/date-sports-mens.jpg', value: 'sports', description: 'navy pinstripe tracksuit'
+      }, {
+      src: '/images/mensSurvey/date-trendy-mens.jpeg', value: 'trendy', description: 'oversized t-shirt over fitted pants'
+    }],
   answer: null
 }, {
-  question: 'What style do you prefer?',
-  images: [{
-    src: '/images/joke-shirt.jpg', value: 'this', description: 'the best t-shirt'
-  }, {
-    src: '/images/joke-shirt.jpg', value: 'that', description: 'the next best t-shirt'
-  }, {
-    src: '/images/joke-shirt.jpg', value: 'the other', description: 'the third best t-shirt'
-  }],
+  question: 'What outfit would you feel most comfortable running errands in?',
+    images: [{
+      src: '/images/mensSurvey/casual-sporty-mens.jpeg', value: 'sporty', description: 't-shirt and trackpants'
+      }, {
+        src: '/images/mensSurvey/casual-trendy-mens.png', value: 'trendy', description: 'black jeans with white t-shirt and denim jacket'
+      }, {
+        src: '/images/mensSurvey/casual-classic-mens.jpg', value: 'classic', description: 'tailored pants with tidy t-shirt'
+      }, {
+        src: '/images/mensSurvey/casual-bold-mens2.jpg', value: 'bold', description: 'mens white onsie with rainbow accent'
+    }],
   answer: null
 }]
