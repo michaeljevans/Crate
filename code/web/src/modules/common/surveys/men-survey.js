6275d0c0b31f194741cd61a1bbea0234d44091aa
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-14 15:58:40 -0600

    Create new file for mens style survey

---
+++ code/web/src/modules/common/surveys/men-survey.js
@@ -0,0 +1,31 @@
+export const mensSurvey = [{
+  question: 'Which outfit would make you feel the most confident at work?',
+  images: [{
+    src: '/images/joke-shirt.jpg', value: 'this', description: 'the best t-shirt'
+  }, {
+    src: '/images/joke-shirt.jpg', value: 'that', description: 'the next best t-shirt'
+  }, {
+    src: '/images/joke-shirt.jpg', value: 'the other', description: 'the third best t-shirt'
+  }],
+  answer: null
+}, {
+  question: 'Which style would you most likely wear on a date night?',
+  images: [{
+    src: '/images/joke-shirt.jpg', value: 'this', description: 'the best t-shirt'
+  }, {
+    src: '/images/joke-shirt.jpg', value: 'that', description: 'the next best t-shirt'
+  }, {
+    src: '/images/joke-shirt.jpg', value: 'the other', description: 'the third best t-shirt'
+  }],
+  answer: null
+}, {
+  question: 'What style do you prefer?',
+  images: [{
+    src: '/images/joke-shirt.jpg', value: 'this', description: 'the best t-shirt'
+  }, {
+    src: '/images/joke-shirt.jpg', value: 'that', description: 'the next best t-shirt'
+  }, {
+    src: '/images/joke-shirt.jpg', value: 'the other', description: 'the third best t-shirt'
+  }],
+  answer: null
+}]
