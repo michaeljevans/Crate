60910e9dcae98c9d550b9a55738293f59286457c
Stephanie Norton <steph.jane.norton@gmail.com>
2020-10-14 20:08:54 -0600

    Add content to womens survey

---
+++ code/web/src/modules/common/surveys/women-survey.js
@@ -0,0 +1,37 @@
+export const womensSurvey = [{
+  question: 'Which outfit would make you feel the most confident at work?',
+  images: [{
+    src: '/images/womensSurvey/work-bold-women.jpg', value: 'bold', description: 'yellow striped blouse with checkered blue pants'
+  }, {
+    src: '/images/womensSurvey/work-classic-women.jpg', value: 'classic', description: 'classic light grey powersuit'
+  }, {
+    src: '/images/womensSurvey/work-sporty-women.jpg', value: 'sporty', description: 'olive green short sleeve shirt with matching loose pants'
+  }, {
+    src: '/images/womensSurvey/work-trendy-women.jpg', value: 'trendy', description: 'black jeans with business casual blazer'
+  }],
+  answer: null
+}, {
+  question: 'Which outfit would you most likely wear on a date night?',
+  images: [{
+    src: '/images/womensSurvey/date-trendy-women.jpg', value: 'trendy', description: 'ripped denim jeans with black off the shoulder top'
+  }, {
+    src: '/images/womensSurvey/date-bold-women.png', value: 'bold', description: 't-shirt dress with sparkly sequins'
+  }, {
+    src: '/images/womensSurvey/date-sporty-women.png', value: 'sporty', description: 'red loose fit tracksuit pants with fitted white tank top'
+  }, {
+    src: '/images/womensSurvey/date-classic-womens.jpg', value: 'classic', description: 'black mid-length dress with polka-dots'
+  }],
+  answer: null
+}, {
+  question: 'What outfit would you feel most comfortable running errands in?',
+  images: [{
+    src: '/images/womensSurvey/casual-classic-women.jpg', value: 'classic', description: 'black slacks with a white t-shirt'
+  }, {
+    src: '/images/womensSurvey/casual-trendy-women.png', value: 'trendy', description: 'black jeans with white t-shirt and denim jacket'
+  }, {
+    src: '/images/womensSurvey/casual-bold-women.jpg', value: 'bold', description: 'dress with long sleeves and a bold pattern'
+  }, {
+    src: '/images/womensSurvey/casual-sporty-women.jpeg', value: 'sporty', description: 'red tracksuit'
+  }],
+  answer: null
+}]
