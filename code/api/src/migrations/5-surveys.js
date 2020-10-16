a7b4f70ad28ee05c79cf89d7a9b29b9c06ea49cb
Michael Evans <michaeljevans113@gmail.com>
2020-10-16 16:29:05 -0600

    Create Survey migration
    Co-authored-by: Josh Tukman <57816551+Joshua-Tukman@users.noreply.github.com>
    Co-authored-by: Dorion <60492885+sciencefixion@users.noreply.github.com>

---
+++ code/api/src/migrations/5-surveys.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('surveys', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      result: {
+        type: Sequelize.TEXT
+      },
+      surveyContents: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('surveys');
+  }
+}
