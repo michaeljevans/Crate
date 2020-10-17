5df83a1de0405058b320c02b6bca4cb2f6eeb5c3
Greyson Elkins <greysonelkins@gmail.com>
2020-10-17 14:03:44 -0600

    Fix missed conflict

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -3,11 +3,7 @@ import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
 import { UserType } from './types'
-<<<<<<< HEAD
-import { create, remove, updateUser} from './resolvers'
-=======
 import { create, remove, updateUserStyle } from './resolvers'
->>>>>>> Working on style update
 
 // Create
 export const userSignup = {
@@ -31,30 +27,8 @@ export const userSignup = {
   resolve: create
 }
 
-<<<<<<< HEAD
-// Remove
-export const userRemove = {
-=======
 // Update
 export const userStyleUpdate = {
->>>>>>> Working on style update
-  type: UserType,
-  args: {
-    id: {
-      name: 'id',
-      type: GraphQLInt
-    }
-  },
-<<<<<<< HEAD
-  resolve: remove
-=======
-  resolve: updateUserStyle
->>>>>>> Working on style update
-}
-
-<<<<<<< HEAD
-//Update
-export const userUpdate = {
   type: UserType,
   args: {
     id: {
@@ -66,7 +40,17 @@ export const userUpdate = {
       type: GraphQLString
     }
   },
-  resolve: updateUser
+  resolve: updateUserStyle
 }
-=======
->>>>>>> Working on survey resolver
+
+export const userRemove = {
+  type: UserType,
+  args: {
+    id: {
+      name: 'id',
+      type: GraphQLInt
+    }
+  },
+  resolve: remove
+}
+
